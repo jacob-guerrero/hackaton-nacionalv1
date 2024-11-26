16baa05c9ad695797460415967676eeaf92763ba
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-11-26 16:22:28 -0500

    Cambiar imagenes

--- src/assets/imgs/assetsManager.tsx
+++ src/assets/imgs/assetsManager.tsx
@@ -1,11 +1,11 @@
 import img1 from "./img1.jpg";
 import img2 from "./img2.jpeg";
-import imgend1 from "./imgend1.jpeg";
-import imgend2 from "./imgend2.jpeg";
-import imgend3 from "./imgend3.jpeg";
-import imgfin1 from "./imgfin1.jpeg";
+import imgend1 from "./imgend1.jpg";
+import imgend2 from "./imgend2.jpg";
+import imgend3 from "./imgend3.jpg";
+import imgfin1 from "./imgfin1.jpg";
 import imgfin2 from "./imgfin2.jpeg";
-import imgfin3 from "./imgfin3.jpeg";
+import imgfin3 from "./imgfin3.jpg";
 import imgfin4 from "./imgfin4.jpeg";
 import logo from "./logo.png";
 
